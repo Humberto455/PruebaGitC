2b33d5beef632d7aac8a5d9f09cd358e6dcc0a52
Humberto455 <hrasgado3892@ppsc.edu.mx>
2022-03-06 16:18:57 -0600

    resta

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -11,6 +11,20 @@ namespace PruebaGit
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tOperaciones!");
+            Operaciones op1 = new Operaciones();
+
+            op1.Resta(10,5);
+        }
+    }
+    class Operaciones
+    {
+        int resultado;
+
+        public void Resta(int valor1,int valor2)
+        {
+            resultado = valor1 - valor2;
+
+            Console.WriteLine("El resultado de la resta es: " + resultado);
         }
     }
 }
